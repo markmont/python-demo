9c0f720a5dd2b1e45feceb86692eef93d520b31c
Mark Montague <markmont@umich.edu>
2022-07-05 11:40:34 -0400

    add the date because it is interesting!

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import sys
+import time
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello from Python! " + sys.version;
+    return "Hello from Python! " + sys.version + "<br />" + (time.strftime('%m/%d/%Y %H:%M:%S'));
 
 app.run(host='0.0.0.0', port=8080)
